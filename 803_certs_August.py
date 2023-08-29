b67018da04d7a9bc9944fe149ace4cb5a17f7fd0
Jesus G <113757515+py3user22@users.noreply.github.com>
2023-08-29 16:19:14 -0500

    Add files via upload
    082923 backup of certs dictionary demo for python3.

---
+++ 803_certs_August.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+
+aug23_cert = {
+    "080323": "Fundamentals of Red Hat Enterprise Linux",
+    "Red Hat": "PEJEPJJYUGN9 | https://coursera.org/share/70bc20912b5979c37630a9890bf3a995",
+
+    "081323": "Linux System Administration w/ IBM Power Systems",
+    "Red.Hat": "8XZ63BNMAEA2 | https://coursera.org/share/6318eec2fc18061d9f88d2e2cf41c02f",
+
+    "RedHat": "Private Cloud Mgmt on IBM power systems",
+    "082723": " | ",
+
+    "082323": "Network Security Fundamentals",
+    "Palo Alto": "VMHQNBVVZPLM | https://coursera.org/share/0b472e8a6164dc00828cc94c5ebffe43",
+
+    "081423": "Machine Learning Operations MLOps -limited event",
+    "Microsoft": "badge | event071723-081423 at learn.microsoft.com",
+
+    "090123": "Cybersecurity foundation",
+    "Palo_Alto": "FDHKQGYXQC69 | https://coursera.org/share/9c2c57122aac8497ebf8d70218509e55",
+
+    "082823": "Security concepts & initial configuration",
+    "Juniper": " | ",
+
+}
+
+for aa, bb in aug23_cert.items():
+    print("{}: {}".format(aa, bb))
+
+print("\n<----------------->")
+
+
+"""
+Linux and Private Cloud Administration on IBM Power Systems Specialization
+    |-----> Fundamentals of Red Hat Enterprise Linux            # 080323' 
+    |-----> Linux System Administration w/ IBM Power Systems    # 081323'
+    |_____> Private Cloud Management on IBM Power Systems       # X
+"""
---
+++ 803_special_cert_all.py
@@ -0,0 +1,131 @@
+#!/bin/python3
+
+
+# Completed
+"""Networking in Google Cloud Specialization                                    # Complete! 072223'
+    |_____> 1- Google Cloud Fundamentals: Core Infrastructure                   # 061923'C
+    |_____> 2- Networking in Google Cloud: Defining and Implementing Networks   # 050623'C
+    |_____> 3- Networking in Google Cloud: Hybrid Connectivity and Network Management   # 072123'C
+
+    RQP6LQ2HK89P | https://coursera.org/share/0f82452c7f6ab084cce55dd999befc9c
+"""
+
+# Completed
+"""Architecting with Google Kubernetes Engine Specialization            # Complete! 062523'
+    |_____> Google Cloud Fundamentals: Core Infrastructure              # 061923'C
+    |_____> Architecting with Google Kubernetes Engine: Foundations     # 061223'C
+    |_____> Architecting with Google Kubernetes Engine: Workloads       # 060923'C
+    |_____> Architecting with Google Kubernetes Engine: Production      # 062523'C
+
+    EXNSBKNTSBCG | https://coursera.org/share/e2b3198aacf43f1bd9c23c4435725705
+"""
+
+# Completed
+"""IBM Cybersecurity Analyst Professional Certificate             # Complete! 052322'C
+    |____> 1- Introduction to Cybersecurity Tools & Cyber Attacks           # 052222'C
+    |____> 2- Cybersecurity Roles, Processes & Operating System Security    # 041522'C
+    |____> 3- Cybersecurity Compliance Framework & System Administration    # 121922'C
+    |____> 4- Network Security & Database Vulnerabilities                   # 050322'C
+    |____> 5- Penetration Testing, Incident Response and Forensics          # 020923'C
+    |____> 6- Cyber Threat Intelligence                                     # 051322'C
+    |____> 7- Cybersecurity Capstone: Breach Response Case Studies          # 051822'C
+    |____> IBM Cybersecurity Analyst Assessment                             # 052422'C
+
+    MG92CETCQVSV | https://coursera.org/share/88e497f9ed82ffa914ee2776de6732f8   
+"""
+
+# Completed
+"""IBM Security Analyst Fundamentals Specialization               # Complete! 051922'
+    |____> 1- Penetration Testing, Incident Response and Forensics          # 020923'C
+    |____> 2- Cyber Threat Intelligence                                     # 051322'C
+    |____> 3- Cybersecurity Capstone: Breach Response Case Studies          # 051822'C
+    
+    RL5NR4AG978U | https://coursera.org/share/0f7f2381a7599df50111265d6b9fc2cd
+"""
+
+# Completed
+"""Google IT Automation with Python Professional Certificate        # Complete! 110722'C
+    |____> 1- Crash Course on Python                                # 
+    |____> 2- Using Python to Interact with the Operating System    # 
+    |____> 3- Introduction to Git and GitHub                        # 
+    |____> 4- Troubleshooting and Debugging Techniques              # 
+    |____> 5- Configuration Management and the Cloud                # 
+    |____> 6- Automating Real-World Tasks with Python               # 110622'C
+    
+    3CZ5GHUZCUQ2 | https://coursera.org/share/fd5169a4b23e65f4a4fa30e2de5cc64b
+"""
+
+# Completed 082723'
+"""Palo Alto Networks Cybersecurity Professional Certificate
+    |_____> Palo Alto Networks Cybersecurity Foundation             # 082723' completed
+    |_____> Palo Alto Networks Network Security Fundamentals        # 082323' completed
+    |_____> Palo Alto Networks Cloud Security Fundamentals          # 042623' completed
+    |_____> Palo Alto Networks Security Operations Fundamentals     # 051223' completed
+
+    26NZLFFT846U | https://coursera.org/share/d71aafa5da30d16667771c3b1d18c8b1
+"""
+
+""" Red Hat
+Linux and Private Cloud Administration on IBM Power Systems Specialization
+    |-----> Fundamentals of Red Hat Enterprise Linux            # 080323' completed
+    |-----> Linux System Administration w/ IBM Power Systems    # 081323' completed
+    |_____> Private Cloud Management on IBM Power Systems       # inprog
+"""
+
+# -----------------------------------------
+
+"""Microsoft Azure Fundamentals AZ-900 Exam Prep Specialization
+    |_____> 1- Introduction to Microsoft Azure Cloud Services               #
+    |_____> 2- Microsoft Azure Management Tools and Security Solutions      # 042923'C
+    |_____> 3- Microsoft Azure Services and Lifecycles                      #
+    |_____> 4- Preparing for the AZ-900 Microsoft Azure Fundamentals Exam   #
+"""
+
+"""John Hopkins university
+    Healthcare IT Support Specialization
+    |_____> Critical role of IT support staff in healthcare            # X
+    |_____> Health IT fundamentals                                     # X
+    |_____> Operations & patient safety for healthcare IT staff        # X
+    |_____> Healthcare data security, privacy, & complaince            # X
+"""
+
+"""Microsoft Azure Developer Associate (AZ-204) Professional Certificate
+    |_____> 1- Create Serverless applications                              #
+    |_____> 2- Connect Your Services with Microsoft Azure Service Bus      #
+    |_____> 3- Data Storage in Microsoft Azure for Associate Developers    #
+    |_____> 4- Deploy a website with Azure Virtual Machines                #
+    |_____> 5- Manage Resources in Azure                                   #
+    |_____> 6- Deploy a website to Azure with Azure App Service            #
+    |_____> 7- Secure your Cloud Data                                      # 051823'C
+"""
+
+"""ISC2 Certified in Cybersecurity Specialization
+    |_____> 1- Security Principles                  # 
+    |_____> 2- Incident Response, BC,& DR concepts  # 
+    |_____> 3- Access Control Concepts              # 
+    |_____> 4- Network Security                     # 071223'C
+    |_____> 5- Security Operations                  # 
+"""
+
+"""Juniper Networks Security Fundamentals Specialization
+    |_____> 1- Juniper Networks Security Concepts and Initial Configuration     # 
+    |_____> 2- Security Features and Advanced Threat Prevention                 # 052623'C
+    |_____> 3- Security Platforms, IPsec, and Troubleshooting                   # 
+"""
+
+""" InfoSec
+Computer Forensics Specialization
+    |_____> 1- Digital Forensics Concepts       # 
+    |_____> 1- Windows OS Forensics             # 
+    |_____> 1- Windows Registry Forensics       # 070823'C
+"""
+
+""" InfoSec
+Python for Cybersecurity Specialization
+    |_____> 1- Introduction to Python for Cybersecurity                     # 071623'C
+    |_____> 2- Execution, persistence, privilege escalation and evasion     #
+    |_____> 3- Credential Access, discovery, lateral movement & collection  #
+    |_____> 4- Python for Command-and-control, Exfiltration and Impact      #
+    |_____> 5- Python for Active Defense                                    #
+
+"""
